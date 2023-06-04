adcc33933c2c3abfa522e7d11449b6bec8e26631
Eran Bibi <eran@infralight.co>
2023-06-04 19:24:32 +0300

    adding new monitor

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,20 @@ resource "datadog_monitor" "new_monitor_for_the_demo" {
   tags = ["foo:bar", "team:fooBar"]
 }
 
+resource "datadog_monitor" "new_monitor_for_the_demo_1" {
+  name               = "AAA Platform Eng Meetup"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
+
+  monitor_thresholds {
+    warning  = 2
+    critical = 4
+  }
+
+  include_tags = true
+
+  tags = ["foo:bar", "team:fooBar"]
+}
